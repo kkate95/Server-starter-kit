17edec442a771fe65ade133818e3582c5ad79b34
kkate95 <kkate95@yandex.ru>
2018-03-17 16:58:16 +0300

    express validation

--- server-starter-kit/app.js
+++ server-starter-kit/app.js
@@ -8,7 +8,8 @@ let express = require('express'),
 let routes = require('./routes/index');
 
 let Err = require('./utils/errors'),
-    BaseError = require('./utils/errors/BaseError');
+    BaseError = require('./utils/errors/BaseError'),
+    ev = require('express-validation');
 
 
 let app = express();
@@ -24,15 +25,50 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*******************************************ROUTES*******************************/
+/* *********************************** ROUTES ****************************** */
 app.use('/', routes);
 
 app.use(function(req, res, next) {
     next(new Err.NotFound('ERR_ROUTE_NOT_FOUND'));
 });
+/* ************************************************************************* */
 
+ev.options({
+    allowUnknownBody: false,
+    allowUnknownQuery: false,
+    allowUnknownParams: false
+});
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
+    if (err instanceof ev.ValidationError) {
+        logger.error(
+            Object.assign(
+                {},
+                err,
+                {
+                    ip:         req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+                    route:      `*** ->>  ${req.method} ${req.path}  <<- ***`,
+                    headers:    req.headers,
+                    params:     req.params,
+                    query:      req.query,
+                    body:       req.body,
+                    user:       req.user
+                },
+                {
+                    error1: err.errors[0],
+                    error2: err.errors[1],
+                    error3: err.errors[2],
+                    error4: err.errors[3]
+                }
+            )
+        );
+        return next(new Err.BadRequest('ERR_BAD_REQUEST'));
+    }
+
+    next(err);
+});
+
+app.use((err, req, res, next) => {
     logger.error(err);
 
     if (!(err instanceof BaseError)) {
